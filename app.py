3da35a9a3feb83a130ad4f1864739271121dd7e8
Brian Ross <brian.ross@students.makeschool.com>
2020-11-15 00:54:37 -0500

    Working image filter

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ import os
 import random
 import requests
 
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-from flask_wtf.csrf import CSRFProtect
+# from flask_wtf import FlaskForm
+# from wtforms import StringField
+# from wtforms.validators import DataRequired
+# from flask_wtf.csrf import CSRFProtect
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
-csrf = CSRFProtect(app)
-csrf.init_app(app)
+# app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
+# csrf = CSRFProtect(app)
+# csrf.init_app(app)
 @app.route('/')
 def homepage():
     """A homepage with handy links for your convenience."""
@@ -160,15 +160,18 @@ def image_filter():
         
         # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
         # as a variable
-        filter_type = request.args.get("filter_type")
+        filter_type = request.form.get("filter_type")
         
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
+        new_file_path = save_image(image, filter_type)
 
         # TODO: Call `apply_filter()` on the file path & filter type
+        apply_filter(new_file_path, filter_type)
+
 
         image_url = f'/static/images/{image.filename}'
 
@@ -176,6 +179,8 @@ def image_filter():
             # TODO: Add context variables here for:
             # - The full list of filter types
             # - The image URL
+            "filter_types": filter_types,
+            "image": image_url
 
         }
 
@@ -184,6 +189,8 @@ def image_filter():
     else: # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
+            "filter_types": filter_types
+
         }
         return render_template('image_filter.html', **context)
 
